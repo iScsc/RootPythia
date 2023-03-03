ab8c88b32f455062410047c236ca2c2198586de9
ctmbl <mabileau.clement@gmail.com>
2023-03-03 11:35:40 +0100

    Add .env loading by docker to run.sh script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build --file Dockerfile -t pyflag-bot:latest .
-docker run --rm --interactive --tty pyflag-bot:latest
+docker run --rm --interactive --tty --env-file .env pyflag-bot:latest
--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+from os import getenv
+
+SECRET = getenv('USELESS_SECRET')
+
 def main():
     print("hello world from pyflag")
+    print("here is my secret from env var:", SECRET)
 
 
 if __name__ == '__main__':
