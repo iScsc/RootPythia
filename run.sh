6719214e873e540abb290fbef2a78518e74589e0
ctmbl <mabileau.clement@gmail.com>
2023-03-03 21:20:27 +0100

    Update run.sh to better handle subcommands

--- run.sh
+++ run.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
-[ "$#" -ne 1 ] && { echo "Please give exactly one argument"; exit 1; }
-
-if [ "$1" == "prod" ]; then
+run__prod () {
 	# prod mode
 	docker build --file Dockerfile -t pyflag-bot:latest .
 	docker run --rm --interactive --tty --env-file .env.prod pyflag-bot:latest
-	exit 0
-fi
+}
 
-if [ "$1" == "dev" ]; then
+run__dev () {
 	# dev mode
 	docker build --file Dockerfile-dev -t pyflag-bot-dev:latest .
 	docker run --rm --interactive --tty --env-file .env.dev --volume ./src:/opt/pyflag-bot/src pyflag-bot-dev:latest
-	exit 0
-fi
+}
 
-echo "'$1' is an unkown command please only use 'prod' or 'dev'"
+run() {
+	local subcmd=$1; shift
+	if type "run__$subcmd" &>/dev/null; then
+		"run__$subcmd" "$@"
+	else
+		echo "subcommand '$subcmd' not recognized" >&2
+		echo "please only use 'prod' or 'dev'" >&2
+		exit 1
+	fi
+}
+
+[ "$#" -ne 1 ] && { echo "Please give exactly one argument"; exit 1; }
+run "$@"
