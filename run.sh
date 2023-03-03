15514a8b6cd4b64292c66d8eda1bfc40e8652969
ctmbl <mabileau.clement@gmail.com>
2023-03-03 20:49:57 +0100

    Add dev mode, separate it from prod mode

--- run.sh
+++ run.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-docker build --file Dockerfile -t pyflag-bot:latest .
-docker run --rm --interactive --tty --env-file .env pyflag-bot:latest
+[ "$#" -ne 1 ] && { echo "Please give exactly one argument"; exit 1; }
+
+if [ "$1" == "prod" ]; then
+	# prod mode
+	docker build --file Dockerfile -t pyflag-bot:latest .
+	docker run --rm --interactive --tty --env-file .env.prod pyflag-bot:latest
+	exit 0
+fi
+
+if [ "$1" == "dev" ]; then
+	# dev mode
+	docker build --file Dockerfile-dev -t pyflag-bot-dev:latest .
+	docker run --rm --interactive --tty --env-file .env.dev --volume ./src:/opt/pyflag-bot/src pyflag-bot-dev:latest
+	exit 0
+fi
+
+echo "'$1' is an unkown command please only use 'prod' or 'dev'"
--- src/main.py
+++ src/main.py
@@ -3,12 +3,15 @@ from os import getenv
 import discord
 
 SECRET = getenv('USELESS_SECRET')
+MODE = getenv('MODE')
 
 def main():
     print("hello world from pyflag")
     print("here is my secret from env var:", SECRET)
 
     print("I also have imported pip package discord.py see `discord.__dir__()`:", discord.__dir__())
+
+    print(f"And we currently are in {MODE} mode")
 
 
 if __name__ == '__main__':
