45e0298ce814e60e0fbe8df0f59a24bcb32232e6
Xlitoni <vacherquentin.47@gmail.com>
2023-03-05 22:40:36 +0100

    GetChallengeById added to the ApiRootMe

--- src/api/fetch.py
+++ src/api/fetch.py
@@ -9,3 +9,21 @@ class ApiRootMe() :
 
     def __init__(self) -> None:
         self.url = "https://api.www.root-me.org/"
+        self.API_KEY = getenv("API_KEY_ROOTME")
+
+    def GetChallengeById(self,id) :
+        """
+        Get a challenge from the API
+        -> returns the raw json for now
+        """
+        cookies = {"api_key": self.API_KEY.strip('"') }
+        resp = requests.get(f"https://api.www.root-me.org/challenges/{id}", cookies=cookies)
+        #raise exception if the request does not work
+        if resp.status_code != 200:
+            raise Exception("GET /challenges/{id}".format(resp.status_code))
+        # take response as json
+        data = resp.json()
+        
+        return(data)
+
+
