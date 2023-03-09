b7eba81ea17bed1f92bbcc58e335ec7cd82d76ba
Xlitoni <vacherquentin.47@gmail.com>
2023-03-09 17:51:15 +0100

    add code to test the pngmakre, works only with PYTHONPATH env variable set and should maybe be set in the dockerfile (would also make it easier to access assets having the absolute path to the ./src/ folder)

--- src/pngmaker/new_validated_challenge.py
+++ src/pngmaker/new_validated_challenge.py
@@ -1,10 +1,16 @@
 from PIL import Image,ImageDraw,ImageFont
 
+from classes.challenge import Challenge
+from classes.user import User
+
 class NewValidatedChallenge() :
 
     def __init__(self,user,challenge) -> None:
-        self.image = Image.open("assets/bg_dark.jpg")
+        self.image = Image.open("/opt/root-pythia/src/assets/bg_dark.jpg")
 
 
+user = User(471176,"Xlitoni",5660,733)
+chall = Challenge(5,"HTML - Code Source","Web - Serveur","Rien de bien difficile",5,"Très facile")
+new_chall_obj = NewValidatedChallenge(user,chall)
 
-
+new_chall_obj.image.save("output.png")
