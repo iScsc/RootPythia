28df078f165e969d6aba4bc96002b109404a8405
Xlitoni <vacherquentin.47@gmail.com>
2023-03-10 16:55:01 +0100

    first working version of the Root-me API with rate limitation (a bit overkill but at least it's there)

--- src/api/rate_limiter.py
+++ src/api/rate_limiter.py
@@ -1,7 +1,70 @@
+import asyncio
+import requests
+import uuid
+
+from datetime import datetime
+
+
+class RequestEntry(object) :
+    """
+    Class for request object to put in the queue for the rate limiter
+    """
+    def __init__(self,url,cookies,key,method) -> None:
+        self.url = url
+        self.cookies = cookies
+        self.key = key
+        self.method = method
 
 class RateLimiter() :
     """
     Class that takes care of sending the requests at a maximum rate (25/sec) 
         and giving back the result to the calling functions when it is received (async probably)
     """
-    pass
+    def __init__(self) :
+        self.requests = {}
+        self.queue = asyncio.Queue()
+        asyncio.create_task(self.handle_requests())
+
+
+    async def handle_requests(self) :
+        print(f"Starting rate_limiter task...")
+        while True :
+            #take a new request from the queue
+            request = await self.queue.get()
+            print(f"[{datetime.now().strftime('%H:%M:%S')}] Treating item in queue : {request.key} -> {request.url} + {request.cookies} ")
+
+            # wait 50ms for rate limitation purpose ;)
+            await asyncio.sleep(0.050)
+
+            if request.method == "GET" :
+                resp = requests.get(request.url, cookies=request.cookies)
+                if resp.status_code != 200 :
+                    raise Exception(f"GET {request.url} -> {resp.status_code}")
+                data = resp.json()
+            else : 
+                raise Exception("Only GET method implemented for now.")
+
+            # we send back the response and trigger the event of this request
+            self.requests[request.key]['result'] = data
+            self.requests[request.key]['event'].set()
+            # finally we inform the queue of the end of the process
+            self.queue.task_done()
+
+    async def make_request(self,url,cookies,method) :
+        key = uuid.uuid4().hex
+
+        print(f"[{datetime.now().strftime('%H:%M:%S')}] Request for {url} added to queue -> {key}")
+
+        event = asyncio.Event()
+        self.requests[key] = {}
+        self.requests[key]['event'] = event
+        request = RequestEntry(url, cookies, key, 'GET')
+        await self.queue.put(request)
+        await event.wait()
+        
+        result = self.requests[key]['result']
+        
+        del self.requests[key]
+
+        return result
+
--- src/api/rootme_api.py
+++ src/api/rootme_api.py
@@ -1,5 +1,6 @@
 from os import getenv
 
+from api.rate_limiter import RateLimiter
 import requests
 import time
 
@@ -9,6 +10,32 @@ class RootMeApi() :
      -> needs a rate_limiter object to send all the requests at a maximum rate of 25 req/sec
     """
 
-    def __init__(self) :
+    def __init__(self,rate_limiter : RateLimiter) :
+        if getenv("API_KEY_ROOTME") is not None :
+            self.API_KEY = getenv("API_KEY_ROOTME")
+        else :
+            raise Exception("API_KEY_ROOTME is not set.")
         self.url = "https://api.www.root-me.org/"
-        self.API_KEY = getenv("API_KEY_ROOTME")
+        self.rate_limiter = rate_limiter
+    
+    async def GetChallengeById(self,id) :
+        """
+        Get a challenge from the API
+        -> returns the raw json for now
+        """
+        #use the api_key in the cookies
+        cookies = {"api_key": self.API_KEY.strip('"') }
+        # ask the rate limiter for the request
+        data = await self.rate_limiter.make_request(f"https://api.www.root-me.org/challenges/{id}",cookies,"GET")
+        return(data)
+    
+    async def GetUserById(self,id) :
+        """
+        Get a user from the API
+        -> returns the raw json for now
+        """
+        #use the api_key in the cookies
+        cookies = {"api_key": self.API_KEY.strip('"') }
+        # ask the rate limiter for the request
+        data = await self.rate_limiter.make_request(f"https://api.www.root-me.org/auteurs/{id}",cookies,"GET")
+        return(data)
