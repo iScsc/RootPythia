8a8191168811f424860fc3de217eb1c7f3f4db12
xlitoni <vacherquentin@outlook.fr>
2023-04-30 23:02:36 +0200

    added max_retry env variable to retry on failed API request

--- src/api/rate_limiter.py
+++ src/api/rate_limiter.py
@@ -3,6 +3,7 @@ import requests
 import uuid
 
 from datetime import datetime
+from os import getenv
 
 
 class RequestEntry(object) :
@@ -20,30 +21,57 @@ class RateLimiter() :
     Class that takes care of sending the requests at a maximum rate (25/sec) 
         and giving back the result to the calling functions when it is received (async probably)
     """
+    _max_retry = 0
+
     def __init__(self) :
         self.requests = {}
         self.queue = asyncio.Queue()
+        #set a max_retry cap
+        if getenv("MAX_API_RETRY") is not None :
+            self._max_retry = int(getenv("MAX_API_RETRY"))
+        else :
+            self._max_retry = 3
+
         asyncio.create_task(self.handle_requests())
 
 
     async def handle_requests(self) :
         print(f"Starting rate_limiter task...")
+        
+        #local variable
         last_time_request = datetime.now()
+        retry = False
+        retry_count = 0
+        
         while True :
             #take a new request from the queue
-            request = await self.queue.get()
-            print(f"[{datetime.now().strftime('%H:%M:%S')}] Treating item in queue : {request.key} -> {request.url} + {request.cookies} ")
+            if retry :
+                #request stays the same
+                print(f"[{datetime.now().strftime('%H:%M:%S')}] Retrying {retry_count} item in queue : {request.key} -> {request.url} + {request.cookies} ")
+                retry = False
+            else :
+                request = await self.queue.get()
+                print(f"[{datetime.now().strftime('%H:%M:%S')}] Treating item in queue : {request.key} -> {request.url} + {request.cookies} ")
+                retry_count = 0
 
             # wait 50ms for rate limitation purpose ;)
             if 0.050 - (datetime.now() - last_time_request) > 0.01 :
                 await asyncio.sleep(0.050 - (datetime.now() - last_time_request))
 
             if request.method == "GET" :
+                # keep track of the last time a request was made
                 last_time_request = datetime.now()
+                # actually send the GET request
                 resp = requests.get(request.url, cookies=request.cookies)
                 if resp.status_code != 200 :
-                    raise Exception(f"GET {request.url} -> {resp.status_code}")
-                data = resp.json()
+                    print(f"[{datetime.now().strftime('%H:%M:%S')}] Request : GET {request.url} + {request.cookies} failed with response code {resp.status_code}")
+                    if retry_count < self._max_retry :
+                        retry = True
+                        retry_count += 1
+                    else :
+                        raise Exception("Looks like a ban to me :'(")
+                else :     
+                    data = resp.json()
             else : 
                 raise Exception("Only GET method implemented for now.")
 
