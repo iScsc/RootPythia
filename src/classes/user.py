c09d155c246babb77e24a432d073b7882e566687
ctmbl <mabileau.clement@gmail.com>
2023-05-25 23:28:10 +0200

    Improve User class to store the number of solves and new solves

--- src/classes/user.py
+++ src/classes/user.py
@@ -6,10 +6,20 @@ class User() :
             idx: int,
             username: str,
             score :int,
-            rank: int
+            rank: int,
+            solves: int,
         """
         parsed_data = User.parse_rootme_user_data(data)
-        (self.idx, self.username, self.score, self.rank) = parsed_data
+
+        # see keys and parse_rootme_user_data for attribute list and values
+        for key, value in parsed_data.items():
+            setattr(self, key, value)
+
+        self.nb_new_solves = 0
+
+    @staticmethod
+    def keys():
+        return ["idx", "username", "score", "rank", "nb_solves"]
 
     @staticmethod
     def parse_rootme_user_data(data):
@@ -19,8 +29,18 @@ class User() :
         username = data["nom"]
         score = int(data["score"])
         rank = int(data["position"])
+        nb_solves = len(data["validations"])
 
-        return (idx, username, score, rank)
+        keys = User.keys()
+        return {keys[0]: idx, keys[1]: username, keys[2]: score, keys[3]: rank, keys[4]: nb_solves}
 
     def __repr__(self):
-        return f"User: id={self.idx}, username={self.username}, score={self.score}, rank={self.rank}"
+        return f"User: id={self.idx}, username={self.username}, score={self.score}, rank={self.rank}, solves={self.nb_solves}"
+
+    def update_new_solves(self, raw_user_data):
+        parsed_data = User.parse_rootme_user_data(raw_user_data)
+
+        self.nb_new_solves = parsed_data["nb_solves"] - self.nb_solves
+
+    def has_new_solves(self):
+        return self.nb_new_solves != 0
