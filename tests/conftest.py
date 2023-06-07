845b51578455b5124c2acfd545349e162668f981
ctmbl <mabileau.clement@gmail.com>
2023-06-07 10:34:40 +0200

    Remove dead code from conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import types
 import logging
 
 import pytest
@@ -16,14 +15,6 @@ pytest_plugins = ["pytest_mock", "pytest_asyncio"]
 
 def pytest_configure():
     pytest.dummy = "dummy global variable"
-
-
-@pytest.fixture
-def demo_fixture():
-    """This fixture is a demo fixture"""
-    obj = types.SimpleNamespace()
-    obj.demo_attr = 2
-    return obj
 
 
 @pytest.fixture
