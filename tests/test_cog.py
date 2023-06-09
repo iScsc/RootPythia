8660f66559da0c0769d3c82586986e0d107f71ba
ctmbl <mabileau.clement@gmail.com>
2023-06-09 23:59:46 +0200

    Add a DISABLED test for the check_new_solves loop: will havr to be fixed

--- tests/test_cog.py
+++ tests/test_cog.py
@@ -1,6 +1,11 @@
+import copy
+
 import pytest
 import discord.ext.test as dpytest
 from asyncio import sleep as asleep
+
+from pngmaker import NewValidatedChallenge
+from data import auteurs_example_data
 
 
 @pytest.mark.asyncio
@@ -40,6 +45,33 @@ async def test_getuser_not_found(config_bot):
     # the .peek() call is needed to not remove the message from queue in order to verify it twice
     assert dpytest.verify().message().peek().contains().content("42")
     assert dpytest.verify().message().contains().content("isn't in the database")
+
+
+# FIXME: this test is disabled because for now dpytest seems not to be able to verify files/images
+# or I just don't know how to use it?
+@pytest.mark.asyncio
+async def NOP_test_check_solves_loop(config_bot, mock_rootme_api_manager, mocker):
+    bot = config_bot
+    rootme_api_manager = mock_rootme_api_manager
+
+    # changing the check_new_solves loop delay interval to speed up the test
+    cog = bot.get_cog(pytest.COG_NAME)
+    cog.check_new_solves.change_interval(seconds=0.1)
+    # disable the file removing in NewValidatedCHallenge.__exit__
+    mocker.patch("pngmaker.NewValidatedChallenge.__exit__")
+
+    # Trigger test
+    await dpytest.message("!adduser 1")
+
+    # change auteurs data: fake a new solve from root me API
+    user_data = copy.deepcopy(auteurs_example_data)
+    user_data["validations"].append({"id_challenge": "-1"})
+    rootme_api_manager.get_user_by_id.return_value = user_data
+
+    await asleep(0.15)
+
+    assert dpytest.verify().message().file(NewValidatedChallenge.NEW_SOLVE_PATH)
+    assert dpytest.verify().message().attachment(NewValidatedChallenge.NEW_SOLVE_PATH)
 
 
 @pytest.mark.asyncio
