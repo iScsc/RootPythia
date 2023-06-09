d46efb4085bbec3696be921466e000525c748c5d
ctmbl <mabileau.clement@gmail.com>
2023-06-09 23:12:56 +0200

    Add error handling test for command and loop errors

--- tests/test_cog.py
+++ tests/test_cog.py
@@ -1,5 +1,6 @@
 import pytest
 import discord.ext.test as dpytest
+from asyncio import sleep as asleep
 
 
 @pytest.mark.asyncio
@@ -39,3 +40,32 @@ async def test_getuser_not_found(config_bot):
     # the .peek() call is needed to not remove the message from queue in order to verify it twice
     assert dpytest.verify().message().peek().contains().content("42")
     assert dpytest.verify().message().contains().content("isn't in the database")
+
+
+@pytest.mark.asyncio
+async def test_command_exception_handling(config_bot, mocker):
+    bot = config_bot
+    # patching this method in particular is arbitrary: the only goal is to raise an exception during a command execution
+    # I chose the "getuser" command purely arbitrary and this could be changed in the future
+    mocker.patch('bot.dummy_db_manager.DummyDBManager.get_user', side_effect = Exception)
+
+    # Trigger test
+    with pytest.raises(Exception):
+        await dpytest.message("!getuser 1")
+
+    assert dpytest.verify().message().content("Command failed, please check logs for more details")
+
+
+@pytest.mark.asyncio
+async def test_loop_exception_handling(config_bot, mocker):
+    bot = config_bot
+    # patching "DummyDBManager.get_userS" here!
+    mocker.patch('bot.dummy_db_manager.DummyDBManager.get_users', side_effect = Exception)
+    # changing the check_new_solves loop delay interval to speed up the test
+    cog = bot.get_cog(pytest.COG_NAME)
+    cog.check_new_solves.change_interval(seconds=0.1)
+
+    # Trigger test
+    await asleep(0.15)
+
+    assert dpytest.verify().message().content("check_new_solves loop failed, please check logs for more details")
