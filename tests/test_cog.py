584d8d8530af46fbb0780cb5e955b91de319cac0
ctmbl <mabileau.clement@gmail.com>
2023-06-14 10:14:47 +0200

    Fix pylint for tests/test_cog.py
    tests/test_cog.py:13:4: W0612: Unused variable 'bot' (unused-variable)
    tests/test_cog.py:27:4: W0612: Unused variable 'bot' (unused-variable)
    tests/test_cog.py:42:4: W0612: Unused variable 'bot' (unused-variable)
    tests/test_cog.py:74:11: E1101: Instance of 'VerifyMessage' has no 'file' member (no-member)
    tests/test_cog.py:80:4: W0612: Unused variable 'bot' (unused-variable)
    tests/test_cog.py:5:0: C0411: standard import "from asyncio import sleep as asleep" should be placed before "import pytest" (wrong-import-order)
    tests/test_cog.py:8:0: C0411: third party import "from data import auteurs_example_data" should be placed before "from pngmaker import NewValidatedChallenge" (wrong-import-order)

--- tests/test_cog.py
+++ tests/test_cog.py
@@ -1,17 +1,14 @@
+from asyncio import sleep as asleep
 import copy
 
 import pytest
 import discord.ext.test as dpytest
-from asyncio import sleep as asleep
 
-from pngmaker import NewValidatedChallenge
 from data import auteurs_example_data
 
 
 @pytest.mark.asyncio
 async def test_adduser_command(config_bot):
-    bot = config_bot
-
     # if the API Manager is not rightly mocked this test should fail, on purpose!
     # because the API Manager is mocked to always return data from tests/data/rootme_example_data
     # no matter the id passed
@@ -24,8 +21,6 @@ async def test_getuser_command(config_bot):
     ###
     # comments are the same than for test_adduser_command you should check it out
     ###
-    bot = config_bot
-
     await dpytest.message("!adduser 42")
     await dpytest.message("!getuser 1")
 
@@ -39,8 +34,6 @@ async def test_getuser_not_found(config_bot):
     ###
     # comments are the same than for test_adduser_command you should check it out
     ###
-    bot = config_bot
-
     await dpytest.message("!getuser 42")
 
     # the .peek() call is needed to not remove the message from queue in order to verify it twice
@@ -71,13 +64,13 @@ async def NOP_test_check_solves_loop(config_bot, mock_rootme_api_manager, mocker
 
     await asleep(0.15)
 
-    assert dpytest.verify().message().file(NewValidatedChallenge.NEW_SOLVE_PATH)
-    assert dpytest.verify().message().attachment(NewValidatedChallenge.NEW_SOLVE_PATH)
+    # can't figure out hwo to do that:
+    #assert dpytest.verify().message().file(NewValidatedChallenge.NEW_SOLVE_PATH)
+    #assert dpytest.verify().message().attachment(NewValidatedChallenge.NEW_SOLVE_PATH)
 
 
 @pytest.mark.asyncio
 async def test_command_exception_handling(config_bot, mocker):
-    bot = config_bot
     # patching this method in particular is arbitrary: the only goal is to raise an exception
     # during a command execution
     # I chose the "getuser" command purely arbitrary and this could be changed in the future
