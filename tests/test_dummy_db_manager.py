d7a197ff1842d4a81e3983955e6dbfd5ab0dc636
ctmbl <mabileau.clement@gmail.com>
2023-06-14 10:16:16 +0200

    Fix pylint for tests/test_dummy_db_manager.py
    tests/test_dummy_db_manager.py:11:79: C0303: Trailing whitespace (trailing-whitespace)
    tests/test_dummy_db_manager.py:17:11: C0123: Use isinstance() rather than type() for a typecheck. (unidiomatic-typecheck)

--- tests/test_dummy_db_manager.py
+++ tests/test_dummy_db_manager.py
@@ -8,13 +8,13 @@ async def test_add_user(mock_dummy_db_manager):
     db = mock_dummy_db_manager
 
     # Trigger test
-    # the user 0 doesn't exists on Root Me, and doesn't correspond to the id of 
+    # the user 0 doesn't exists on Root Me, and doesn't correspond to the id of
     # tests/data/rootme_api_example_data
     # so if the API Manager isn't mocked correctly this should raise an error
     ret_user = await db.add_user(0)
 
     assert ret_user is not None
-    assert type(ret_user) == User
+    assert isinstance(ret_user, User)
     assert ret_user.username == "g0uZ"  # heavily dependant on test data!
 
 
